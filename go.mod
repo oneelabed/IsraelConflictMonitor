fa6a8dac57c4e4a5ca7ed1153d5d290188fabe6b
oneelabed <oneelabed123@gmail.com>
2026-03-02 17:44:00 +0200

    setting up the chi router and http server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/oneelabed/RSSAggregator
 
 go 1.25.0
+
+require (
+	github.com/go-chi/chi v1.5.5 // direct
+	github.com/go-chi/cors v1.2.2 // direct
+)
--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 func main() {
-	fmt.Println("g")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not found in env")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1router := chi.NewRouter()
+	v1router.Get("/healthz", handlerReadiness)
+	v1router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	fmt.Printf("Server starting on port %v\n", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
