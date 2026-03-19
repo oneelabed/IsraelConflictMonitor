05f64c7c36cccaadb978277064d75d9155cfb949
oneelabed <oneelabed123@gmail.com>
2026-03-19 11:27:04 +0200

    changed middleware file name to proxy due to deprecation

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const apiKey = request.cookies.get('api_key')?.value;
   const role = request.cookies.get('role')?.value;
   const { pathname } = request.nextUrl;
